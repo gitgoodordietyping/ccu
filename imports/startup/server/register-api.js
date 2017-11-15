3cc506d0e0c8082091b2f72f1a81f769a24af077
gitgoodordietyping <a2fmendes@gmail.com>
2017-11-15 17:49:04 +0000

    basic energy plan done, added examples to db at startup, fixed some imports in server

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,1 +1,5 @@
 // Register your apis here
+import '../../api/energy_plans/methods.js';
+import '../../api/houses/methods.js';
+import '../../api/subscriptions/methods.js';
+import '../../api/users/methods.js';
